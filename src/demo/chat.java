ed4cda9981296e77c2e8f29842619ba3d3e66f04
ryuichi1998 <152033b@mymail.nyp.edu.sg>
2016-11-28 17:25:35 +0800

    Initial commit

---
+++ src/demo/chat.java
@@ -0,0 +1,63 @@
+package demo;
+
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@ServerEndpoint("/chat/{id}")
+public class chat {
+    private static Set<Session> allSessions = new HashSet<Session>();
+    @OnOpen
+    public void register(@PathParam("id") String id, Session session){
+        System.out.println("Registering id : " + id + "for session: " + session.getId());
+        session.getUserProperties().put("id", id);
+        allSessions.add(session);
+        try {
+            session.getBasicRemote().sendText(id + " registered successfully!");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+
+    @OnMessage
+    public void onMessage(String txt, Session session) throws IOException {
+        System.out.println("Received : " + txt);
+        //allSessions = session.getOpenSessions();
+        System.out.println(session.getOpenSessions().size());
+        for (Session s: allSessions) {
+            try{
+                String msg = (String)session.getUserProperties().get("id") + " >> " + txt;
+                s.getBasicRemote().sendText(msg);
+                System.out.println("Sent : " + msg);
+            } catch (IOException ioe) {
+                System.out.println(ioe.getMessage());
+            }
+        }
+    }
+
+
+    @OnClose
+    public void onClose(CloseReason reason, Session session) {
+        allSessions.remove(session);
+        System.out.println("Closing a WebSocket due to " + reason.getReasonPhrase());
+
+    }
+
+    
+    public void getAllConnectedUsers(@PathParam("id") String id, Session session){
+        session.getUserProperties().put("id", id);
+        allSessions.add(session);
+        try {
+            session.getBasicRemote().sendText(id);
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 152033b
+  Date: 11/28/2016
+  Time: 2:46 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
